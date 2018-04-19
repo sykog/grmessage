511190a82efc3fe6fccb93ce0c6038437b06faa6
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-04-19 16:26:53 -0400

    updated registration page, added javascript file (Antonio & Pavel)

---
+++ javascript/register.js
@@ -0,0 +1,9 @@
+// adds a listener to window.onload
+$(document).ready(function() {
+
+    // button variables
+    var student = $("button#student");
+	var instructor = $("button#instructor");
+
+	student
+});
