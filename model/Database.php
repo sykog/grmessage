3791a7d4ea5376784ea1d8e1f535e13521991821
Sam Ourn <sourn@mail.greenriver.edu>
2018-04-26 13:14:23 -0400

    Added some database functions

--- model/Database.php
+++ model/Database.php
@@ -16,10 +16,59 @@ class Database
         try {
             // instantiate a PDO object
             $this->dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-            echo 'Connected to database!';
+
         }
         catch (PDOException $e) {
             echo $e->getMessage();
         }
     }//end constructor
-}
+
+    function addStudent($email, $password, $phone, $fname, $lname, $carrier)
+    {
+        $dbh = $this->dbh;
+        // define the query
+        $sql = "INSERT INTO students(studentEmail, password, phone, fname, lname)
+          VALUES (:email, :password, :phone, :fname, :lname, :carrier)";
+
+        // prepare the statement
+        $statement = $dbh->prepare($sql);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->bindParam(':phone', $phone, PDO::PARAM_STR);
+        $statement->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $statement->bindParam(':lname', $lname, PDO::PARAM_STR);
+        $statement->bindParam(':carrier', $carrier, PDO::PARAM_STR);
+
+        // execute
+        $statement->execute();
+    }//end addStudent
+
+    function addInstructor($email, $password,$fname, $lname)
+    {
+        $dbh = $this->dbh;
+        // define the query
+        $sql = "INSERT INTO instructors(email, password, fname, lname)
+          VALUES (:email, :password, :fname, :lname)";
+
+        // prepare the statement
+        $statement = $dbh->prepare($sql);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $statement->bindParam(':lname', $lname, PDO::PARAM_STR);
+
+        // execute
+        $statement->execute();
+    }//end addInstructor
+
+    function changeStudentPassword($id, $newPassword)
+    {
+        $dbh = $this->dbh;
+        // define the query
+        $sql = "UPDATE students
+          SET (password = :newPassword)
+          WHERE studentid = :id";
+
+    }//end changeStudentPassword
+
+}//end Database class
