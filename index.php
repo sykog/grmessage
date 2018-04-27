03b75876e44585bc1f8274c3e986ecab1cd2cd8e
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-04-27 12:49:07 -0400

    fixed the message route

--- index.php
+++ index.php
@@ -26,9 +26,12 @@ $f3->route('GET|POST /', function() {
 
 // define a route for registration
 $f3->route('GET|POST /register', function($f3, $params) {
+    $database = new Database();
     $errors = array();
-    if(isset($_POST['submit'])){
-        $email = $_POST['email'];
+
+    // if registering as a student
+    if(isset($_POST['submitS'])){
+        $email = $_POST['semail'];
         $password = $_POST['password'];
         $confirm = $_POST['confirm'];
         $first = $_POST['first'];
@@ -71,21 +74,95 @@ $f3->route('GET|POST /register', function($f3, $params) {
         $f3->set('errors', $errors);
         $f3->set('success', $success);
     }
+     // if registering as an instructor
+    if(isset($_POST['submitI'])){
+        $email = $_POST['iemail'];
+        $password = $_POST['password'];
+        $confirm = $_POST['confirm'];
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $phone = $_POST['phone'];
+        $carrier = $_POST['carrier'];
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        $_SESSION['confirm'] = $confirm;
+        $_SESSION['first'] = $first;
+        $_SESSION['last'] = $last;
+        $_SESSION['phone'] = $phone;
+        $_SESSION['carrier'] = $carrier;
+        if(!validIEmail($email)){
+            $errors['iemail'] = "Please enter an instructor email.";
+        }
+        if(!validPassword($password)){
+            $errors['password'] = "Password is less than 6 characters.";
+        }
+        if(!validConfirm($password, $confirm)){
+            $errors['confirm'] = "Password and confirmation do not match.";
+        }
+        if(!validPhone($phone)){
+            $errors['phone'] = "Invalid phone number.";
+        }
+        if(!validCarrier($carrier)) {
+            $errors['phone'] = "Invalid carrier.";
+        }
+        $success = sizeof($errors) == 0;
+        $f3->set('email', $email);
+        $f3->set('password', $password);
+        $f3->set('confirm', $confirm);
+        $f3->set('first', $first);
+        $f3->set('last', $last);
+        $f3->set('phone', $phone);
+        $f3->set('carrier', $carrier);
+        $f3->set('errors', $errors);
+        $f3->set('success', $success);
+    }
+
     $template = new Template();
     echo $template->render('views/registration.html');
     if($success) {
         //$f3->reroute('/profile');
-        echo "success";
+        // if student account
+        if ($email == $_POST['semail']) {
+            // only submit if the email doesnt exist
+            if($database->studentExists($email)) {
+                echo "email already exists";
+            } else {
+                $database->addStudent($email, $password, $phone, $first, $last, $carrier);
+                echo "Student registered!";
+            }
+        }
+        // if instructor account
+        if ($email == $_POST['iemail']) {
+            // only submit if the email doesnt exist
+            if($database->instructorExists($email)) {
+                echo "email already exists";
+            } else {
+                $database->addInstructor($email, $password, $first, $last);
+                echo "Instructor registered!";
+            }
+        }
     }
 });
 
-// define a default route
-$f3->route('GET /testConnection', function() {
-
+// define a message route
+$f3->route('GET|POST /message', function($f3, $params) {
     $dbh = new Database(DB_DSN,DB_USERNAME, DB_PASSWORD);
+    if(isset($_POST['submit'])){
+        $textMessage = $_POST['textMessage'];
+        $textMessage = trim($textMessage);
+        $f3->set('textMessage', $textMessage);
+        if(validTextMessage ($textMessage)){
+            $to = "2536531125@vtext.com";
+            $headers = "From: LaterGators\n";
+            mail($to, '', $textMessage ."\n", $headers);
+            //echo $textMessage."length".strlen($textMessage);
+        }
+        else{
+            echo "must be between 1 and 250 characters";
+        }
+    }
     $template = new Template();
-    echo $template->render('views/registration.html');
-
+    echo $template->render('views/instructorMessage.html');
 });
 
 $f3->route('POST /login', function($f3) {
@@ -102,6 +179,7 @@ $f3->route('POST /login', function($f3) {
     }
 
 });
+
 
 // run fat free
 $f3->run();
