2aea460e1143ab0b9d13be4ea8ee00696618a0e6
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-05-01 12:25:06 -0400

    changed styles throughout

--- javascript/changePassword.js
+++ javascript/changePassword.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
     //change password button is clicked
     $("#change").click(function() {
 
-        $("#change").hide();
+        //$("#change").hide();
         $("#changePassword").html("<h3>Change Password</h3>" +
             "<p><strong>Current Password: </strong> <input type='password' name='currentPassword'></p>" +
             "<p><strong>New Password: </strong><input type='password' name='newPassword'></p>" +
             "<p><strong>Confirm New Password: </strong><input type='password' name='confirmPassword'></p>" +
-            "<input type=\"submit\" name=\"changePassword\" value=\"Change Password\" class=\"btn btn-success align-bottom\">");
+            "<button type=\"submit\" name=\"changePassword\">Change Password</button>");
 
     });
 });
