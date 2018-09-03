f01d20252a1c52f05e0a73d555064c3ce36780b7
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-09-03 16:22:30 -0400

    improved styles on registration, send message, and view messages tables

--- js/messageTable.js
+++ js/messageTable.js
@@ -2,9 +2,29 @@
 
 // data table is sorted by newest messages first
 $(document).ready(function() {
+
+    // adds more search pfeatures to filter by program
+    $.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+        var min = parseInt( $('#min').val(), 10 );
+        var max = parseInt( $('#max').val(), 10 );
+        var age = parseFloat( data[3] ) || 0; // use data for the age column
+
+        if ( ( isNaN( min ) && isNaN( max ) ) ||
+             ( isNaN( min ) && age <= max ) ||
+             ( min <= age   && isNaN( max ) ) ||
+             ( min <= age   && age <= max ) )
+        {
+            return true;
+        }
+        return false;
+    });
+
+    // initialize data table
     var table = $('#messagesTable').DataTable({
         "order": [[ 3, "desc" ]],
         "columnDefs": [
+            { "width": "10%", "targets": 0 },
             { "width": "10%", "targets": 3 },
             { "width": "18%", "targets": 2 }
         ]
